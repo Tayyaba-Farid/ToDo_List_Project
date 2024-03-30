203de08ccb25841041e24f9f82c288730905c7cb
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-03-30 21:44:57 +0500

    ToDoList Project

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let todos = [];
+let condition = true;
+while (condition) {
+  let addinList = await inquirer.prompt([
+    {
+      name: "todo",
+      message: "what do you want to add in your Todos list?",
+      type: "input",
+    },
+    {
+      name: "addMore",
+      message: "are you sure you want to add more in Todos list?",
+      type: "confirm",
+      default: "false",
+    },
+  ]);
+  todos.push(addinList.todo);
+  condition = addinList.addMore
+ console.log(todos);
+}
